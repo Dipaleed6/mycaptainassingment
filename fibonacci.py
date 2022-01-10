d92823f560fabb3e4ccd3264115ee7982d2da875
Dipaleed6 <95572756+Dipaleed6@users.noreply.github.com>
2022-01-10 13:31:20 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 15 15:43:21 2021
-
-@author: Nithyashri
-"""
-
-"""
-Write a Python Program for Fibonacci numbers 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
-"""
-
 a=0
 b=1
 no=0
